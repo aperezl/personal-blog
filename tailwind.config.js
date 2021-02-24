0f0c2c8f6642d8699a0f36547e63206d39613041
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-24 17:47:49 +0100

    update taildwind and postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,mdx}', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
